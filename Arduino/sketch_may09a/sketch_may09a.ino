d0101b82bcd91ba4166523ceff8ed199fee1460b
christianp9 <39104944+christianp9@users.noreply.github.com>
2019-05-09 12:10:57 -0500

    Add files via upload

---
+++ Arduino/sketch_may09a/sketch_may09a.ino
@@ -0,0 +1,16 @@
+int inpu;
+void setup() {
+  pinMode(13, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  if(Serial.available() > 0){
+    inpu = Serial.read();
+    if(inpu == '1'){
+      digitalWrite(13, HIGH);
+      }else{
+        digitalWrite(13, LOW);
+        }
+    }
+}
